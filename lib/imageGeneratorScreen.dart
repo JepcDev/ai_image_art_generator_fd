a4d44014d2173d29c4d13912d9976017e9bf5249
JepcDev <joseenrique.pc21@gmail.com>
2025-03-27 11:00:25 -0400

    feat: Generate images in flutter with API POST method from imagine.art

--- lib/imageGeneratorScreen.dart
+++ lib/imageGeneratorScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
 class ImageGeneratorScreen extends StatefulWidget {
   const ImageGeneratorScreen({super.key});
 
@@ -9,6 +11,26 @@ class ImageGeneratorScreen extends StatefulWidget {
 }
 
 class _ImageGeneratorScreenState extends State<ImageGeneratorScreen> {
+  TextEditingController controller = TextEditingController();
+
+  generateImage(var prompt) async {
+
+    var request = http.MultipartRequest('POST', Uri.parse("https://api.vyro.ai/v2/image/generations"));
+    request.headers['Authorization'] = "Bearer vk-B9fE8RCDK6VhTF25liTuFNcwHarn2il0otgj3NLCuRDL3M7";
+    request.fields["prompt"] = prompt;
+    request.fields["style"] = "realistic";
+    request.fields["aspect_ratio"] = "1:1";
+    request.fields["seed"]= "0";
+
+    var response = await request.send();
+
+    if (response.statusCode ==200) {
+      print("image generated successfully");
+    }else{
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +42,9 @@ class _ImageGeneratorScreenState extends State<ImageGeneratorScreen> {
         Icon(Icons.ac_unit,size: 250,),
         Row(
           children: [
-            Expanded(child: TextField()),
+            Expanded(child: TextField(controller: controller,)),
             InkWell(child: Icon(Icons.send), onTap: (){
-              
+              generateImage(controller.text);
             },)
           ],
         )
---
+++ lib/info.md
@@ -0,0 +1,5 @@
+# INFO
+  Pagina para generar imagenes con ia [Link imagine.art](https://www.imagine.art/).<br>
+  API [imagine.art](https://www.imagine.art/api)  
+  Imagine art [tex-to-image](https://reference.imagine.art/api-10672910)
+  Package de dart que estamos usando para hacer las peticiones para generar las imagenes [http](https://pub.dev/packages/http/install)
